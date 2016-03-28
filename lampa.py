ac0ea1402f44a2ba6b7a50fc3684a74cb3f465fb
husvagnspc <basenmobil82@gmail.com>
2016-03-28 14:58:42 +0200

    Create lampa.py

---
+++ lampa.py
@@ -0,0 +1,7 @@
+#Kodi python skript för att starta lamp styrning.
+#Gör ett skript per lampa
+#Fungerar endast ihop med styrningarna
+import os
+import xbmc
+
+os.system("sudo python /opt/skript/lampstyrning/lampa.py");
