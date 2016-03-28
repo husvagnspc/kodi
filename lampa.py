f4ba1d441c0bb2e5661037ec98432f6e4a68c333
husvagnspc <basenmobil82@gmail.com>
2016-03-28 14:59:23 +0200

    Skapat lampa.py
    Lampstyrning via kodi

--- lampa.py
+++ lampa.py
@@ -1,6 +1,7 @@
 #Kodi python skript för att starta lamp styrning.
 #Gör ett skript per lampa
 #Fungerar endast ihop med styrningarna
+#Skapat av Andreas Olsson för Husvagns/husbils pc projektet
 import os
 import xbmc
 
